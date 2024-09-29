714113759dae25b834dc391ff47b4ddad6da931b
CanweiHe <fvoidcn@gmail.com>
2024-09-29 10:46:17 +0800

    init repo.

---
+++ study/study3.6.py
@@ -0,0 +1,15 @@
+import torch
+from IPython import display
+from d2l import torch as d2l
+
+batch_size = 512
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+num_inputs = 784
+num_outputs = 10
+
+W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
+X = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+print(X.sum(0, keepdim=True), X.sum(1, keepdim=True))

f6d37908bace0706776b2b059efd9d4fdb6c3257
fv <fvoidcn@gmail.com>
2024-11-21 08:58:51 +0800

    finish study 4.3

---
+++ study/study4.3.py
@@ -0,0 +1,96 @@
+import torch
+from d2l import torch as d2l
+
+
+class Classifier(d2l.Module):  # @save
+    """The base class of classification models."""
+
+    def validation_step(self, batch):
+        Y_hat = self(*batch[:-1])
+        self.plot('loss', self.loss(Y_hat, batch[-1]), train=False)
+        self.plot('acc', self.accuracy(Y_hat, batch[-1]), train=False)
+
+
+@d2l.add_to_class(d2l.Module)  # @save
+def configure_optimizers(self):
+    return torch.optim.SGD(self.parameters(), lr=self.lr)
+
+
+@d2l.add_to_class(Classifier)  # @save
+def accuracy(self, Y_hat, Y, averaged=True):
+    """Compute the number of correct predictions."""
+    Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+    preds = Y_hat.argmax(axis=1).type(Y.dtype)
+    compare = (preds == Y.reshape(-1)).type(torch.float32)
+    return compare.mean() if averaged else compare
+
+
+X = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+print(X.sum(0, keepdim=True))
+print(X.sum(1, keepdim=True))
+
+
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdims=True)
+    return X_exp / partition  # The broadcasting mechanism is applied here
+
+
+X = torch.rand((2, 5)) * 3
+print(X)
+X_prob = softmax(X)
+print(X_prob)
+print(X_prob.sum(1))
+
+
+class SoftmaxRegressionScratch(d2l.Classifier):
+    def __init__(self, num_inputs, num_outputs, lr, sigma=0.01):
+        super().__init__()
+        self.save_hyperparameters()
+        self.W = torch.normal(0, sigma, size=(num_inputs, num_outputs),
+                              requires_grad=True)
+        self.b = torch.zeros(num_outputs, requires_grad=True)
+
+    def parameters(self):
+        return [self.W, self.b]
+
+
+@d2l.add_to_class(SoftmaxRegressionScratch)
+def forward(self, X):
+    X = X.reshape((-1, self.W.shape[0]))
+    return softmax(torch.matmul(X, self.W) + self.b)
+
+
+y = torch.tensor([0, 2])
+y_hat = torch.tensor([[0.1, 0.3, 0.6], [0.3, 0.2, 0.5]])
+print(y)
+print(y_hat)
+print(y_hat[[0, 1], y])
+
+
+def cross_entropy(y_hat, y):
+    return -torch.log(y_hat[list(range(len(y_hat))), y]).mean()
+
+
+print(cross_entropy(y_hat, y))
+
+
+@d2l.add_to_class(SoftmaxRegressionScratch)
+def loss(self, y_hat, y):
+    return cross_entropy(y_hat, y)
+
+
+data = d2l.FashionMNIST(batch_size=256)
+model = SoftmaxRegressionScratch(num_inputs=784, num_outputs=10, lr=0.1)
+trainer = d2l.Trainer(max_epochs=10)
+trainer.fit(model, data)
+
+X, y = next(iter(data.val_dataloader()))
+preds = model(X).argmax(axis=1)
+preds.shape
+
+wrong = preds.type(y.dtype) != y
+X, y, preds = X[wrong], y[wrong], preds[wrong]
+labels = [a + '\n' + b for a, b in zip(
+    data.text_labels(y), data.text_labels(preds))]
+data.visualize([X, y], labels=labels)
